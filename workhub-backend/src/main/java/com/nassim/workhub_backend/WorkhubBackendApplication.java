7ced6389b36b913010550620f0264512fec410fe
Nassim <133519075+Bellarab@users.noreply.github.com>
2025-10-22 12:18:01 +0100

    chore: initialize WorkHub project (Angular + Spring Boot setup)

---
+++ workhub-backend/src/main/java/com/nassim/workhub_backend/WorkhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nassim.workhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhubBackendApplication.class, args);
+	}
+
+}
---
+++ workhub-backend/src/test/java/com/nassim/workhub_backend/WorkhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassim.workhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
